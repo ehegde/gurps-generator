bbee14d66d75f2c0a3bc48a378e432bbf6aaba0c
E H <emily.hegde@gmail.com>
2024-11-04 19:55:39 -0500

    fix vite deploy error

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://ehegde.github.io/gurps-generator/",
   server: {
     port: 3000,
   },
