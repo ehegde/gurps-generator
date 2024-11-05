3b42d51d1004ca2a98c13080635c3bd857a009da
E H <emily.hegde@gmail.com>
2024-11-04 20:04:56 -0500

    adjust homepage 3

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://ehegde.github.io/gurps-generator/",
+  base: "/gurps-generator",
   server: {
     port: 3000,
   },
